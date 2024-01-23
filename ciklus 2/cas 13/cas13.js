e12f3d5586bf8b6aeeb200e748798f79becf56dc
Ahmed <ahmedraslj@gmail.com>
2024-01-23 19:42:37 +0100

    .

--- ciklus 2/cas 13/cas13.js
+++ ciklus 2/cas 13/cas13.js
@@ -3,7 +3,7 @@
 // imamo arr.shift() izbacuje nam prvi element iz niza 
 // imamo arr.unshift(el) da ubacimo element na pocetak niza
 
-const test = "ime-prezime";
+// const test = "ime-prezime";
 
 
 
@@ -11,3 +11,15 @@ const test = "ime-prezime";
 // const test2 = test.split("-")
 
 // console.log(test2)
+
+
+const broj = prompt(`Unesite broj n:`);
+
+for (let i = 1; i <= broj; i++) {
+    let str = '';
+    for (let j = 0; j < i; j++){
+        str += '*'
+    }
+    console.log(str);
+    // str = '';
+}
