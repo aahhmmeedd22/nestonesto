f85b7437272fea8b2d27aa895faecad57de62464
Ahmed <ahmedraslj@gmail.com>
2024-01-29 21:07:45 +0100

    .

---
+++ ciklus 2/cas 16/index.js
@@ -0,0 +1,23 @@
+// const nasZadatak = [1,2,3,4,5,6,7,8,9,10]
+
+// let noviNiz = nasZadatak.map((el,i,arr) =>{
+//     if(i % 2 === 0){
+//         return el + 1;
+//     }
+//     else{
+//         return el - 1;
+//     }
+// }
+// )
+
+// console.log(nasZadatak,noviNiz)
+const nasZadatak = [1,2,3,4,5,6,7,8,9,10]
+let noviNIz = []
+
+function nesto(nasZadatak) {
+    for (let i = 0; i < nasZadatak.length; i++) {
+         noviNIz.push(nasZadatak[i] + 1)
+    }
+}
+nesto(nasZadatak)
+console.log(noviNIz)
