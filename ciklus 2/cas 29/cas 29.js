339e97b63f773d67c82d06513dcc532fae9bea9c
Ahmed <ahmedraslj@gmail.com>
2024-03-04 20:36:50 +0100

    .

---
+++ ciklus 2/cas 29/cas 29.js
@@ -0,0 +1,43 @@
+// const arr = [1,2,3,4,5,6,7,8,9]
+
+// const sum = arr.reduce((preValue, currValue)=>{
+//     return preValue + currValue
+// },10)
+// console.log(sum, "sum from reducer");
+
+// let sum2 = 0
+// arr.forEach((el)=> sum2+=el)
+// console.log(sum2);
+
+
+// const knjige = [{ime:"knj1", brStr:200, tip:"roman"}, {ime:"knj1", brStr:120, tip:"sci-fi"}, {ime:"knj1", brStr:220, tip:"roman"}];
+
+// const sumaStrRoman = knjige.reduce((preValue, el)=> {
+//     if (el.tip === "roman") {
+//         return  preValue + el.brStr
+//     }
+//     else{
+//         return preValue
+//     }
+// }, 0)
+
+// console.log(sumaStrRoman);
+
+
+// const kemal = {
+//     ime:"kemal",
+//     prezime:"hrhrhrhr",
+//     godine:23
+// }
+
+// const {ime,...rest} = kemal;
+
+// console.log(rest);
+
+const operator = 2 === 2 ? true : false
+
+console.log(operator);
+
+const amIAMan = "covek" === "zena" ? "jesteCovek" : "nijeCovek"
+
+console.log(amIAMan);
