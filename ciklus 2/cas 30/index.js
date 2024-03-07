1156226d8d9addbfeffb342ee0b3357887460879
Ahmed <ahmedraslj@gmail.com>
2024-03-07 20:52:11 +0100

    .

---
+++ ciklus 2/cas 30/index.js
@@ -0,0 +1,41 @@
+//asihroni js - v8 engine
+// api - aplicaion programming interface 
+
+// const arrObj = [{ime:"ime"}, {ime:"ime2"}]
+
+// const arrObjNovi = {ime:"ime3"}
+
+// arrObj.push(arrObjNovi)
+
+// console.log(arrObj);
+
+// const arrOdKey = [];
+
+// const arrOdValue = []
+
+// const test = {
+//     ime:"neko",
+//     prezime:"neko2",
+//     godine:22,
+//     lokacija:"kosanciceva"
+// }
+// arrOdKey.push(Object.keys(test))
+
+// arrOdValue.push(Object.values(test))
+
+// console.log(arrOdKey);
+// console.log(arrOdValue);
+
+
+// const nekiObj = {
+//     ime:`neko`,
+//     godine:12,
+//     lokacija:"najjaki"
+// }
+
+// Object.entries(nekiObj).forEach(([key])=>{
+//     console.log(key)
+// })
+// Object.entries(nekiObj).forEach(([value])=>{
+//     console.log(value)
+// })
